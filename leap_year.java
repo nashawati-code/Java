5d81feb8ae3f2080a06b72ee841f635f6e24aead
Aya Nashawati <61278736+amn26@users.noreply.github.com>
2020-02-23 21:59:46 +0200

    Create leap_year.java

---
+++ leap_year.java
@@ -0,0 +1,26 @@
+//the following code takes user input year
+//and checks if int year is a leap year
+
+import java.util.Scanner;
+
+public class LeapYear 
+{
+  //======================================================//
+  public static void main(String[] args)
+  {
+      Scanner in = new Scanner(System.in);
+      System.out.print("Input a year: ");
+      int year = in.nextInt();
+      System.out.println(is_LeapYear(year));
+  }
+ //======================================================//
+  public static boolean is_LeapYear(int y)
+  {
+      boolean a = (y % 4) == 0;
+      boolean b = (y % 100) != 0;
+      boolean c = ((y % 100 == 0) && (y % 400 == 0));
+
+      return a && (b || c);
+  }
+  //======================================================//
+}
